d3274333378ab19786642617c18ec06df00c0a31
Heiz <1256831069@qq.com>
2021-01-14 17:56:28 +0800

    tabbar自定义

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,8 +2,25 @@
   <view class="content">
     <image class="logo" src="/static/logo.png"></image>
     <view class="text-area">
-      <text class="title">{{ title }}</text>
+      <text class="title" @click="toggleMessage('info')">{{ title }}</text>
     </view>
+    <!-- 对话框 -->
+    <uni-popup
+      id="popupDialog"
+      ref="popupDialog"
+      type="dialog"
+      @change="change"
+      :maskClick="true"
+    >
+      <uni-popup-dialog
+        :type="msgType"
+        title="通知"
+        content="欢迎使用 uni-popup!"
+        :before-close="true"
+        @confirm="dialogConfirm"
+        @close="dialogClose"
+      ></uni-popup-dialog>
+    </uni-popup>
   </view>
 </template>
 
@@ -13,6 +30,7 @@ export default {
   data() {
     return {
       title: "Hello",
+      msgType: "info",
     };
   },
   onLoad(...options) {
@@ -26,6 +44,39 @@ export default {
       getAreaList("/?r=").then((res) => {
         console.log(res);
       });
+    },
+    /**
+     * 打开消息提示
+     * @param {Object} type
+     */
+    toggleMessage(type) {
+      this.msgType = type;
+      this.$refs.popupDialog.open();
+    },
+    /**
+     * 对话框点击确认按钮
+     */
+    dialogConfirm(done) {
+      // this.$refs.popupMessage.open();
+      console.log("点击确认");
+      // 需要执行 done 才能关闭对话框
+      done();
+    },
+    /**
+     * 对话框取消按钮
+     */
+    dialogClose(done) {
+      this.msgType = "info";
+      this.message = "点击了对话框的取消按钮";
+      // 需要执行 done 才能关闭对话框
+      done();
+    },
+    /**
+     * popup 状态发生变化触发
+     * @param {Object} e
+     */
+    change(e) {
+      console.log("popup " + e.type + " 状态", e.show);
     },
   },
 };
