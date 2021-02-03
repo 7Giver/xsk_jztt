05163e164419b6df3d81c084ac215043c55c1f84
Heiz <1256831069@qq.com>
2021-02-03 17:45:29 +0800

    足迹页面布局

---
+++ pages/mine/footprint.vue
@@ -0,0 +1,84 @@
+<template>
+  <view>
+    <u-navbar title="我的足迹" :is-fixed="true" :border-bottom="false">
+      <view class="navbar-right" slot="right">
+        <u-icon name="more-dot-fill" size="43" color="#333"></u-icon>
+      </view>
+    </u-navbar>
+    <view class="page-footpoint">
+      <u-index-list :scrollTop="scrollTop">
+        <view v-for="(item, index) in indexList" :key="index">
+          <u-index-anchor :index="item" />
+          <view class="list-cell">列表1</view>
+          <view class="list-cell">列表2</view>
+          <view class="list-cell">列表3</view>
+        </view>
+      </u-index-list>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollTop: 0,
+      indexList: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+      ],
+    };
+  },
+  methods: {
+    onPageScroll(e) {
+      this.scrollTop = e.scrollTop;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar-right {
+  margin-right: 30rpx;
+  display: flex;
+}
+.list-cell {
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 10px 24rpx;
+  overflow: hidden;
+  color: #323233;
+  font-size: 14px;
+  line-height: 24px;
+  background-color: #fff;
+}
+
+.anchor-text {
+  color: red;
+}
+</style>
