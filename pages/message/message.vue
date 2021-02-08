0aa5153b1164566123b6a8f1d58f3f7fc749c5bb
Heiz <1256831069@qq.com>
2021-02-08 17:34:09 +0800

    消息页面布局

--- pages/message/message.vue
+++ pages/message/message.vue
@@ -56,7 +56,7 @@
         <view class="message_list">
           <view class="item" v-for="(item, index) in messageList" :key="index">
             <view class="top_wrap">
-              <u-avatar :src="item.avatar"></u-avatar>
+              <u-avatar :src="item.avatar" size="85"></u-avatar>
               <view class="avatar_right">
                 <view class="nikcname">{{item.nickname}}</view>
                 <view class="add_time">{{item.add_time}}</view>
@@ -65,7 +65,7 @@
             <view class="bottom_wrap">
               <view class="comment">{{item.comment}}</view>
               <view class="article_wrap">
-                <image :src="item.banner" mode="widthFix" />
+                <image class="banner" :src="item.banner" />
                 <view class="title">{{item.title}}</view>
               </view>
             </view>
@@ -183,6 +183,61 @@ export default {
             color: #666666;
             font-size: 26rpx;
             line-height: 1.6;
+          }
+        }
+      }
+    }
+    .message_list {
+      .item {
+        padding: 20rpx 0 30rpx;
+        border-bottom: 1px solid#E6E6E6;
+        .top_wrap {
+          display: flex;
+          align-items: center;
+          .u-avatar {
+            margin-right: 20rpx;
+          }
+          .avatar_right {
+            .nikcname {
+              width: 260rpx;
+              font-size: 28rpx;
+              font-weight: bold;
+              text-overflow: -o-ellipsis-lastline;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              display: -webkit-box;
+              -webkit-line-clamp: 1;
+              line-clamp: 1;
+              -webkit-box-orient: vertical;
+              word-break: break-all;
+            }
+            .add_time {
+              color: #999999;
+              font-size: 24rpx;
+            }
+          }
+        }
+        .bottom_wrap {
+          padding-left: 104rpx;
+          .comment {
+            padding: 4rpx 0 16rpx;
+          }
+          .article_wrap {
+            padding: 20rpx;
+            background: #f7f7f7;
+            display: flex;
+            align-items: center;
+            .banner {
+              width: 220rpx;
+              height: 120rpx;
+            }
+            .title {
+              flex: 1;
+              color: #333333;
+              font-size: 24rpx;
+              line-height: 1.6;
+              margin-left: 20rpx;
+            }
           }
         }
       }
