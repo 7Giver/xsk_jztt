edd846902c8f1310c2e1748ce4826af45f02d336
Heiz <1256831069@qq.com>
2021-03-12 18:41:14 +0800

    创作中心布局

---
+++ pages/mine/creative.vue
@@ -0,0 +1,193 @@
+<template>
+  <view id="app">
+    <u-navbar title="创作中心" :border-bottom="false"></u-navbar>
+    <view class="page-creative">
+      <view class="header">
+        <u-avatar :src="vuex_user.avatar" size="60"></u-avatar>
+        <view class="title">欢迎来到你的创作中心</view>
+      </view>
+      <view class="inner_wrap">
+        <view class="item">
+          <view class="title">总阅读量</view>
+          <view class="number">{{688}}</view>
+        </view>
+        <view class="item">
+          <view class="title">总粉丝</view>
+          <view class="number">{{962}}</view>
+        </view>
+        <view class="item">
+          <view class="title">总收益</view>
+          <view class="number">{{2337}}</view>
+        </view>
+      </view>
+      <view class="list_wrap">
+        <view class="main_title">我的作品</view>
+        <view class="data_list">
+          <view class="item" v-for="(item, index) in dataList" :key="index">
+            <view class="left_wrap">
+              <view class="u-line-2 title">{{item.title}}</view>
+              <view class="u-flex bottom">
+                <view class="number">{{item.shares}}转发</view>
+                <view class="number">{{item.comments}}评论</view>
+                <view class="number">{{item.likes}}点赞</view>
+              </view>
+            </view>
+            <view class="right_wrap">
+              <image :src="item.cover" mode="scaleToFill" />
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="footer">
+        <view class="u-flex item">
+          <image class="icon" src="/static/img/mine/icon_wz.png" mode="scaleToFill" />
+          <text>发布文章</text>
+        </view>
+        <view class="u-flex item">
+          <image class="icon" src="/static/img/mine/icon_sp.png" mode="scaleToFill" />
+          <text>发布视频</text>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataList: [
+        {
+          title: "测试对象存储VS的发射点范德萨范德萨",
+          shares: 698,
+          comments: 300,
+          likes: 477,
+          cover: "https://cdn.uviewui.com/uview/swiper/1.jpg",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.page-creative {
+  .header {
+    background: url("/static/img/mine/pic_bg.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100% 100%;
+    display: flex;
+    align-items: center;
+    padding: 40rpx 30rpx 120rpx;
+    color: #fff;
+    .u-avatar {
+      margin-right: 20rpx;
+    }
+    .title {
+      font-size: 32rpx;
+    }
+  }
+  .inner_wrap {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 90%;
+    margin: -90rpx auto 0;
+    padding: 50rpx 40rpx;
+    background: #fff;
+    border-radius: 20rpx;
+    box-shadow: 0px 2px 10px 0px rgba(231, 231, 231, 0.8);
+    .item {
+      text-align: center;
+      .title {
+        color: #999999;
+        font-size: 24rpx;
+      }
+      .number {
+        color: #333;
+        font-size: 34rpx;
+        font-weight: bold;
+        margin-top: 10rpx;
+      }
+    }
+  }
+  .list_wrap {
+    padding: 0 30rpx;
+    .main_title {
+      font-size: 30rpx;
+      font-weight: bold;
+      margin: 40rpx auto 30rpx;
+      line-height: 30rpx;
+      padding-left: 16rpx;
+      border-left: 8rpx solid #f04323;
+    }
+    .data_list {
+      .item {
+        display: flex;
+        padding-bottom: 20rpx;
+        border-bottom: 1px solid #e6e6e6;
+        .left_wrap {
+          display: flex;
+          justify-content: space-between;
+          flex-direction: column;
+          padding-right: 28rpx;
+          .title {
+            font-size: 30rpx;
+          }
+          .bottom {
+            justify-content: space-between;
+            .number {
+              color: #999999;
+              font-size: 26rpx;
+            }
+          }
+        }
+        .right_wrap {
+          flex: 1;
+          image {
+            display: block;
+            width: 260rpx;
+            height: 160rpx;
+          }
+        }
+      }
+    }
+  }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    border-top: 1px solid #E6E6E6;
+    .item {
+      position: relative;
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      line-height: 100rpx;
+      .icon {
+        width: 38rpx;
+        height: 38rpx;
+        margin-right: 10rpx;
+      }
+      text {
+        color: #f04323;
+        font-size: 30rpx;
+      }
+      &:first-child::after {
+        content: "";
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 1px;
+        height: 46rpx;
+        background: #e6e6e6;
+      }
+    }
+  }
+}
+</style>
--- pages/mine/friend.vue
+++ pages/mine/friend.vue
@@ -27,17 +27,29 @@
       </view>
       <view class="gap_wrap">我的好友</view>
       <view class="container">
-        <view class="friend_list">
-          <view class="u-flex item" v-for="(item, index) in dataList" :key="index">
-            <view class="u-flex left_wrap">
-              <u-avatar :src="item.avatar" size="80"></u-avatar>
-              <view class="u-line-1 name_wrap">{{item.name}}</view>
-            </view>
-            <view class="right_wrap">
-              <view class="add_time">{{item.add_time}}</view>
+        <scroll-view
+          :scroll-y="true"
+          style="height: 100%;width: 100%;"
+          @scrolltolower="reachBottom"
+        >
+          <view class="friend_list">
+            <view class="u-flex item" v-for="(item, index) in dataList" :key="index">
+              <view class="u-flex left_wrap">
+                <u-avatar :src="item.avatar" size="80"></u-avatar>
+                <view class="u-line-1 name_wrap">{{item.name}}</view>
+              </view>
+              <view class="right_wrap">
+                <view class="add_time">{{item.add_time}}</view>
+              </view>
             </view>
           </view>
-        </view>
+          <view class="loadmore" v-if="dataList.length">
+            <u-loadmore :status="loadStatus"></u-loadmore>
+          </view>
+          <view class="nodata" v-else>
+            <u-empty text="暂无数据"></u-empty>
+          </view>
+        </scroll-view>
       </view>
     </view>
   </view>
@@ -53,19 +65,31 @@ export default {
       money: 0,
       limit: 20,
       pageIndex: 1,
-      dataList: [
-        {
-          avatar: "",
-          name: "测试昵称",
-          add_time: "2021.3.11 16:50",
-        },
-      ],
+      loadStatus: "loadmore",
+      dataList: [],
     };
   },
   onLoad(options) {
     this.getData();
   },
   methods: {
+    // 触发滚动加载
+    reachBottom() {
+      if (this.dataList.length >= this.limit) {
+        this.loadStatus = "loading";
+        setTimeout(() => {
+          this.getData();
+        }, 500);
+      }
+    },
+    // 监听下拉刷新
+    onPullDownRefresh() {
+      this.limit = this.$options.data().limit;
+      this.pageIndex = this.$options.data().pageIndex;
+      this.loadStatus = this.$options.data().loadStatus;
+      this.dataList = this.$options.data().dataList;
+      this.getData();
+    },
     async getData() {
       let params = {
         token: this.vuex_token,
@@ -76,6 +100,20 @@ export default {
       this.total = data.total;
       this.cash = data.cash;
       this.money = data.money;
+      let result = data.list;
+      if (data == null || result.length == 0) {
+        // 加载结束
+        this.loadStatus = "nomore";
+        return;
+      }
+      this.dataList = this.dataList.concat(result);
+      this.pageIndex++;
+      if (result.length < this.limit) {
+        // 一页不足的情况
+        this.loadStatus = "nomore";
+      } else {
+        this.loadStatus = "loadmore";
+      }
     },
   },
 };
@@ -123,15 +161,14 @@ export default {
     background: #f2f2f2;
   }
   .container {
-    background: #f2f2f2;
+    position: relative;
+    background: #fff;
     .friend_list {
       padding: 0 30rpx;
-      background: #fff;
       .item {
         padding: 20rpx 0;
         justify-content: space-between;
         border-bottom: 1px solid #e6e6e6;
-        background: #fff;
         .left_wrap {
           width: 50%;
           .u-avatar {
@@ -149,6 +186,16 @@ export default {
           }
         }
       }
+    }
+    .loadmore {
+      padding: 20rpx 0;
+    }
+    .nodata {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      zoom: 1.15;
     }
   }
 }
